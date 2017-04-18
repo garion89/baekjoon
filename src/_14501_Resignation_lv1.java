1faedd8a40ed0a7c9a671a48d2c4b9ab8dc0be81
garion89 <garion89@snu.ac.kr>
2017-04-18 16:13:20 +0900

    commits~!

---
+++ src/_14501_Resignation_lv1.java
@@ -0,0 +1,67 @@
+/*
+input
+10
+5 50
+4 40
+3 30
+2 20
+1 10
+1 10
+2 20
+3 30
+4 40
+5 50
+
+output
+90
+ */
+
+import java.util.Scanner;
+
+public class _14501_Resignation_lv1 {
+    public static int N;
+    public static int[] day, money;
+    public static boolean[][] possibleGraph;
+    public static int[][] moneyGraph;
+
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        day = new int[N];
+        money = new int[N];
+        possibleGraph = new boolean[N + 1][N + 1];
+        moneyGraph = new int[N + 1][N + 1];
+
+        for (int i = 0; i < N; i++) {
+            day[i] = sc.nextInt();
+            money[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            possibleGraph[i][i + 1] = true;
+            if (i + day[i] <= N) {
+                possibleGraph[i][i + day[i]] = true;
+                moneyGraph[i][i + day[i]] = money[i];
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = i+1; j <= N; j++) {
+                    if(k <= i || j <= k || !possibleGraph[i][k] || !possibleGraph[k][j])
+                        continue;
+
+                    int temp = moneyGraph[i][k] + moneyGraph[k][j];
+
+                    if(moneyGraph[i][j] <= temp) {
+                        moneyGraph[i][j] = temp;
+                        possibleGraph[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        System.out.println(moneyGraph[0][N]);
+    }
+}
